93c4498266231e7798cc402da27ef31ba137ec0f
Annelise Rocha <annelisesr@gmail.com>
2023-04-05 20:54:43 -0300

    first commit

---
+++ Exemplos_java.java
@@ -0,0 +1,29 @@
+// primitivo: incluem boolean, char, byte, short, int, long, float, e double.
+// não primitivo: incluem string, classes, interfaces e arrays.
+
+public class Exemplos_java{
+    public static void main(String[] args){
+        boolean requisito = false;
+        char letraA = 'A';
+        byte a = -128;
+        short b = 32767;
+        int c = 2000000;
+        long d = 90000000000L;
+        float pi = 3.14f;
+        double salario = 3724.12;
+
+        String nome = "Anne";
+
+        final int idadeDoJogador = 18;
+
+        int um = 3;
+        int dois = 2;
+        int tres = 0;
+
+        tres = um + dois + idadeDoJogador;
+
+        System.out.print("O resultado é" + c);
+    }
+}
+
+
